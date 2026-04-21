0df91d428f092ba2d0c28eca88f416d7a9ba4de6
psw3137 <sunwoo4159@naver.com>
2026-04-21 15:48:49 +0900

    프론트엔드 프로토타입 ver 1.1
    캐시제거

--- android/app/.cxx/Debug/6f2614h5/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "C:/project/golf_frontend/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/r4vh1u2m/obj/armeabi-v7a/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/project/golf_frontend/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
